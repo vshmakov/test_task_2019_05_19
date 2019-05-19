0a4360500ee4e7027261d1ad7f5a97707cbceb36
Vadim Shmakov <shmakovvadim@mail.ru>
2019-05-19 16:28:32 +0500

    [task] Done creating tasks

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -79,52 +79,6 @@ final class TaskController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/{id}", name="task_show", methods={"GET"})
-     */
-    public function show(Task $task): Response
-    {
-        return $this->render('task/show.html.twig', [
-            'task' => $task,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="task_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Task $task): Response
-    {
-        $form = $this->createForm(TaskType::class, $task);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('task_index', [
-                'id' => $task->getId(),
-            ]);
-        }
-
-        return $this->render('task/edit.html.twig', [
-            'task' => $task,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="task_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Task $task): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($task);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('task_index');
-    }
-
     private function createTaskQueryBuilder(EntityManagerInterface $entityManager): QueryBuilder
     {
         return $entityManager
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -6,21 +6,22 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TaskType extends AbstractType
+final class TaskType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('description')
-        ;
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('title', TextType::class);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Task::class,
